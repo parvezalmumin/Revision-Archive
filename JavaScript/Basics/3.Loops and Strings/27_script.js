0c19e6de4492e5392693158d1c296251ce5ac9d3
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-09-20 01:56:47 -0700

    Template Literal Practice code added

---
+++ JavaScript/Basics/3.Loops and Strings/27_script.js
@@ -0,0 +1,33 @@
+let name = "Mumin";
+let age = 23;
+
+let message = `My name is ${name} and I am ${age} years old.`;
+
+console.log(message);
+
+
+console.log(`Next year I will be ${age + 1}`);
+
+
+
+// We Can Put Expressions Inside
+let a = 10;
+let b = 20;
+
+console.log(`The total is ${a + b}`);
+
+
+let firstName = "Parvez";
+let lastName = "Al Mumin";
+let country = "Bangladesh";
+
+let introduction = `My name is ${firstName} ${lastName}. I am from ${country}.`;
+
+console.log(introduction);
+
+
+let message1 = `Hello Masho
+Welcome to JavaScript
+Keep learning`;
+
+console.log(message1);

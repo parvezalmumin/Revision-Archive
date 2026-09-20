3a1222a49101e4496c077458784715c3e8f1c672
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-09-19 23:58:14 -0700

    Another for of loop practise code added

---
+++ JavaScript/Basics/3.Loops and Strings/16_script.js
@@ -0,0 +1,11 @@
+let str = "Javascript Revision with Sradha Miss of Apna College";
+
+let size = 0;
+
+for (let val of str){
+  console.log("value = ", val);
+
+  size++
+}
+
+console.log("string size = ", size)

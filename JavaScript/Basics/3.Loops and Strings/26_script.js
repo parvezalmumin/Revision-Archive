a40af2165f54dc4ea0c20bd3b357038829f82992
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-09-20 01:41:51 -0700

    String Practise Code added

---
+++ JavaScript/Basics/3.Loops and Strings/26_script.js
@@ -0,0 +1,41 @@
+let country = "Bangladesh";
+let message = "Hello World";
+
+console.log(country);
+console.log(message);
+
+// String Can Contain Anything
+let age = "23"; //let age = "23";
+let fullName = "Parvez Al Mumin";
+let symbol = "@#$%";
+let sentence = "I am learning JavaScript.";
+
+console.log(age);
+console.log(fullName);
+console.log(symbol);
+console.log(sentence);
+
+
+// Check the Type
+let name = "Mumin";
+
+console.log(typeof name);
+
+
+// String Length
+let name1 = "Mumin";
+
+console.log(name1.length);
+
+
+// Spaces are also counted
+let name2 = "Parvez Al Mumin";
+
+console.log(name2.length);
+
+//
+let name3 = "Mumin";
+
+console.log(name3[0]);
+console.log(name3[1]);
+console.log(name3[2]);

37da3fde1aa79d84244fdcd2a693996a411e2a34
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-09-20 20:42:42 -0700

    replace - string method practise code added

---
+++ JavaScript/Basics/3.Loops and Strings/40_script.js
@@ -0,0 +1,12 @@
+let message = "I am learning Python";
+let result = message.replace("Python", "JavaScript" );
+console.log(result);
+
+let greet = "Hello";
+console.log(greet.replace("lo","p"));
+
+let greet1 = "Hellolololo";
+console.log(greet1.replace("lo","p"));
+
+let greet2 = "Hellolololo";
+console.log(greet2.replaceAll("lo","p"));

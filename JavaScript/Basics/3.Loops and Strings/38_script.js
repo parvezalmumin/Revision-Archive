532381f21c604a60e6b4ecc86af1d5eeadddb387
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-09-20 20:31:27 -0700

    concate string method practise code added

---
+++ JavaScript/Basics/3.Loops and Strings/38_script.js
@@ -0,0 +1,16 @@
+let firstName = "Al";
+let lastName = "Mumin";
+let fullName = firstName.concat(" ", lastName);
+console.log(fullName);
+
+
+let a = "Java";
+let b = "script";
+let course = a.concat(b);
+console.log(course)
+
+let x = "Parvez ";
+let y = "Al ";
+let z = "Mumin ";
+let q = x + y + z;
+console.log(q);

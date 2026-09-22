c8e42f1648fedf9d0685cc84b7db861e2d5e2411
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-09-22 00:25:18 -0700

    Add JavaScript for...in array index and value examples

---
+++ JavaScript/Basics/4.Arrays/9_script.js
@@ -0,0 +1,13 @@
+// for...in gives the indexes.
+let fruits = ["Apple", "Mango", "Banana"];
+
+for (let index in fruits) {
+    console.log(index);
+}
+
+
+
+// If you want the values:
+for (let index in fruits) {
+    console.log(fruits[index]);
+}

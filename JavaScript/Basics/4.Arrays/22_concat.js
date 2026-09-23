07eb1a8a16d80c6ffbf4d67eea1458297352de5b
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-09-22 21:44:40 -0700

    Arrays Method - Concate Practise code added

---
+++ JavaScript/Basics/4.Arrays/22_concat.js
@@ -0,0 +1,6 @@
+let marvel_heroes = ["Iron Man", "Spider-Man", "Moon Knight","Spider-Noir", "Ghost Rider", "Venom"];
+let dc_heroes = ["Batman", "Superman", "Green Lantern", "Green Arrow", "Nightwing"];
+let deshi_heroes = ["Machineman", "Bijli", "Ibrahim"];
+
+let heroes = dc_heroes.concat(marvel_heroes, deshi_heroes);
+console.log(heroes);

e707b60c86dfec6e04f50e615dd840a177ad89d6
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2026-09-22 21:14:58 -0700

    Arrays String - Concat Practise code added

---
+++ JavaScript/Basics/4.Arrays/20_concat.js
@@ -0,0 +1,8 @@
+let fruits1 = ["Apple", "Banana"];
+let fruits2 = ["Mango", "Orange"];
+
+let allFruits = fruits1.concat(fruits2);
+
+console.log(fruits1);
+console.log(fruits2);
+console.log(allFruits);
